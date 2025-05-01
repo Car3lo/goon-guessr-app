670b7f300f6551a8aacd9a3f4f6a576e17688058
Car3lo <cartilo_odalbu@outlook.com>
2025-05-01 15:36:41 +0800

    baddies until may 10

--- src/data/schedule.ts
+++ src/data/schedule.ts
@@ -11,45 +11,6 @@ interface Schedule {
 }
 
 export const gameSchedule: Schedule = {
-  "2025-04-27": {
-    images: [
-      "https://i.imgur.com/34sK7ft.jpeg",
-      "https://i.imgur.com/uzuX27F.jpeg"
-    ],
-    correctWord: "Lea Martinez",
-    socialMediaUsername: "@slayeas",
-    socialMediaLink: "https://www.instagram.com/slayeas/",
-    placeholder: "Enter your guess (Full Name)"
-  },
-  "2025-04-28": {
-    images: [
-      "https://i.imgur.com/JkvPAqI.jpeg"
-    ],
-    correctWord: "Vera Dijkmans",
-    socialMediaUsername: "@veradijkmans",
-    socialMediaLink: "https://www.instagram.com/veradijkmans",
-    placeholder: "Enter your guess (Full Name)"
-  },
-  "2025-04-29": {
-    images: [
-      "https://pbs.twimg.com/media/GoVPfdBWMAEHByq?format=jpg&name=medium",
-      "https://pbs.twimg.com/media/GooRwDwXMAAKjzs?format=jpg&name=large"
-    ],
-    correctWord: "Sabrina Carpenter",
-    socialMediaUsername: "@sabrinacarpenter",
-    socialMediaLink: "https://www.instagram.com/sabrinacarpenter/",
-    placeholder: "Enter your guess (Full Name)"
-  },
-  "2025-04-30": {
-    images: [
-      "https://i.imgur.com/34sK7ft.jpeg",
-      "https://i.imgur.com/uzuX27F.jpeg"
-    ],
-    correctWord: "Lea Martinez",
-    socialMediaUsername: "@slayeas",
-    socialMediaLink: "https://www.instagram.com/slayeas/",
-    placeholder: "Enter your guess (Full Name)"
-  },
   "2025-05-01": {
     images: [
       "https://i.ibb.co/qLKPq27L/Snap-Insta-to-473740130-18480923038001486-1965616125113370944-n.jpg",
@@ -79,6 +40,80 @@ export const gameSchedule: Schedule = {
     correctWord: "Lea Martinez",
     socialMediaUsername: "@slayeas",
     socialMediaLink: "https://www.instagram.com/slayeas/",
+    placeholder: "Enter your guess (Full Name)"
+  },
+  "2025-05-04": {
+    images: [
+      "https://pbs.twimg.com/media/GpcKBmYW4AAIxDS?format=jpg&name=large",
+      "https://pbs.twimg.com/media/GpcKBkwXUAAlgBk?format=jpg&name=large"
+    ],
+    correctWord: "Thea Thompson",
+    socialMediaUsername: "@theaadoraa",
+    socialMediaLink: "https://www.instagram.com/theaadoraa/",
+    placeholder: "Enter your guess (Full Name)"
+  },
+  "2025-05-05": {
+    images: [
+      "https://i.imgur.com/iFhDeNh.jpeg",
+      "https://i.imgur.com/McrVzKQ.jpeg",
+      "https://i.imgur.com/dJZzhkC.jpeg"
+    ],
+    correctWord: "Izel Palacio",
+    socialMediaUsername: "@ayzelll_",
+    socialMediaLink: "https://www.instagram.com/ayzelll_/",
+    placeholder: "Enter your guess (Full Name)"
+  },
+  "2025-05-06": {
+    images: [
+      "https://i.ibb.co/qLKPq27L/Snap-Insta-to-473740130-18480923038001486-1965616125113370944-n.jpg",
+      "https://i.ibb.co/pv3FdQrF/Snap-Insta-to-483876399-18491527819001486-1017862629739679861-n.jpg",
+      "https://i.ibb.co/3yPqWtN8/Snap-Insta-to-484144194-18490851487001486-3337812738107921353-n.jpg"
+    ],
+    correctWord: "Sheeny Velasco",
+    socialMediaUsername: "@sheenyberry",
+    socialMediaLink: "https://getallmylinks.com/sheenyberry",
+    placeholder: "Enter your guess (Full Name)"
+  },
+  "2025-05-07": {
+    images: [
+      "https://i.ibb.co/wr80W70r/Snap-Insta-to-485451010-18494085925036863-5000970990621119072-n.jpg",
+      "https://i.ibb.co/8Dj0B2fD/Snap-Insta-to-357996497-1673912149714604-3976168879462412990-n.jpg",
+      "https://i.ibb.co/nqbmwtqq/Snap-Insta-to-471850155-575563561911405-8411403499432906787-n.jpg"
+    ],
+    correctWord: "Alyssa Eckenroth",
+    socialMediaUsername: "@alyssaeckenroth",
+    socialMediaLink: "https://www.instagram.com/alyssaeckenroth/",
+    placeholder: "Enter your guess (Full Name)"
+  },
+  "2025-05-08": {
+    images: [
+      "https://scontent.fmnl37-1.fna.fbcdn.net/v/t39.30808-6/492044556_700175525855081_3743634320227882744_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeEce6ZYZhhoGrRbU8TPV-H8UJcyA8CKkrRQlzIDwIqStEIkrA9wacg7UnLj1QHn-jwSu2vlG6XWJHL6-RrhwhXH&_nc_ohc=_4X8rbsvtY0Q7kNvwGbCPPp&_nc_oc=AdkAKjPSWGir00h4KiSMQo2HwcIxwPWoJiLaW0NiRfOx55IQuZ4h7wNnTJAxCjReP5o&_nc_zt=23&_nc_ht=scontent.fmnl37-1.fna&_nc_gid=qxqJjE1edBNh4jH3yLjUHw&oh=00_AfG4WMUH7B5CRDo1OI5i0cR6jGGb5GyGFjxhoF6kJ8Txpw&oe=6818CE15",
+      "https://i.ibb.co/sJJDnZQj/Snap-Insta-to-475308667-18327633814233629-2834385034515290182-n.jpg",
+      "https://i.ibb.co/Fqf0mg0k/Snap-Insta-to-463596978-421964717361631-5911334653358302529-n.jpg"
+    ],
+    correctWord: "Jennie",
+    socialMediaUsername: "@jennierubyjane",
+    socialMediaLink: "https://www.instagram.com/jennierubyjane/?hl=en",
+    placeholder: "Enter your guess (First Name)"
+  },
+  "2025-05-09": {
+    images: [
+      "https://pbs.twimg.com/media/GceW5mzXAAAWLO2?format=jpg&name=large"
+    ],
+    correctWord: "Violet Myers",
+    socialMediaUsername: "@waifuviolet",
+    socialMediaLink: "https://www.instagram.com/waifuviolet/?hl=en",
+    placeholder: "Enter your guess (Full Name)"
+  },
+  "2025-05-10": {
+    images: [
+      "https://pbs.twimg.com/media/Ger7CrpXYAAbdgw?format=jpg&name=large",
+      "https://i.ibb.co/r2wF4yxs/Snap-Insta-to-482621388-18164439751336926-223906374140538726-n.jpg",
+      "https://i.ibb.co/vvkYtNYW/Snap-Insta-to-457396907-3914524452201907-7194868956224132042-n.jpg"
+    ],
+    correctWord: "Ella Netzer",
+    socialMediaUsername: "@ella_netzer8",
+    socialMediaLink: "https://www.instagram.com/ella_netzer8/",
     placeholder: "Enter your guess (Full Name)"
   }
   // Add more dates as needed
