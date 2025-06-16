329a6773f1759749f4dada0f539999cf7732dc03
Car3lo <cartilo_odalbu@outlook.com>
2025-06-16 21:20:12 +0800

    Updated game data from June 16 to July 01

--- src/data/schedule.ts
+++ src/data/schedule.ts
@@ -11,126 +11,130 @@ interface Schedule {
 }
 
 export const gameSchedule: Schedule = {
-  "2025-05-01": {
-    images: [
-      "https://i.ibb.co/qLKPq27L/Snap-Insta-to-473740130-18480923038001486-1965616125113370944-n.jpg",
-      "https://i.ibb.co/pv3FdQrF/Snap-Insta-to-483876399-18491527819001486-1017862629739679861-n.jpg",
-      "https://i.ibb.co/3yPqWtN8/Snap-Insta-to-484144194-18490851487001486-3337812738107921353-n.jpg"
-    ],
-    correctWord: "Sheeny Velasco",
-    socialMediaUsername: "@sheenyberry",
-    socialMediaLink: "https://getallmylinks.com/sheenyberry",
-    placeholder: "Enter your guess (Full Name)"
+  "2025-06-16": {
+    images: ["https://pbs.twimg.com/media/GomcbUTWMAAhIWA?format=jpg&name=large"],
+    correctWord: "Sydney Thomas",
+    socialMediaUsername: "@itssydneythomas",
+    socialMediaLink: "https://www.instagram.com/iamsydneythomas/",
+    placeholder: "Enter your guess"
   },
-  "2025-05-02": {
-    images: [
-      "https://pbs.twimg.com/media/GoVPfdBWMAEHByq?format=jpg&name=medium",
-      "https://pbs.twimg.com/media/GooRwDwXMAAKjzs?format=jpg&name=large"
-    ],
-    correctWord: "Sabrina Carpenter",
-    socialMediaUsername: "@sabrinacarpenter",
-    socialMediaLink: "https://www.instagram.com/sabrinacarpenter/",
-    placeholder: "Enter your guess (Full Name)"
-  },
-  "2025-05-03": {
-    images: [
-      "https://i.imgur.com/34sK7ft.jpeg",
-      "https://i.imgur.com/uzuX27F.jpeg"
-    ],
-    correctWord: "Lea Martinez",
-    socialMediaUsername: "@slayeas",
-    socialMediaLink: "https://www.instagram.com/slayeas/",
-    placeholder: "Enter your guess (Full Name)"
-  },
-  "2025-05-04": {
-    images: [
-      "https://pbs.twimg.com/media/GpcKBmYW4AAIxDS?format=jpg&name=large",
-      "https://pbs.twimg.com/media/GpcKBkwXUAAlgBk?format=jpg&name=large"
-    ],
+  "2025-06-17": {
+    images: ["https://pbs.twimg.com/media/GpcKBmYW4AAIxDS?format=jpg&name=large"],
     correctWord: "Thea Thompson",
     socialMediaUsername: "@theaadoraa",
     socialMediaLink: "https://www.instagram.com/theaadoraa/",
-    placeholder: "Enter your guess (Full Name)"
+    placeholder: "Enter your guess"
   },
-  "2025-05-05": {
-    images: [
-      "https://i.imgur.com/iFhDeNh.jpeg",
-      "https://i.imgur.com/McrVzKQ.jpeg",
-      "https://i.imgur.com/dJZzhkC.jpeg"
-    ],
+  "2025-06-18": {
+    images: ["https://i.imgur.com/McrVzKQ.jpeg"],
     correctWord: "Izel Palacio",
     socialMediaUsername: "@ayzelll_",
     socialMediaLink: "https://www.instagram.com/ayzelll_/",
-    placeholder: "Enter your guess (Full Name)"
+    placeholder: "Enter your guess"
   },
-  "2025-05-06": {
-    images: [
-      "https://i.ibb.co/qLKPq27L/Snap-Insta-to-473740130-18480923038001486-1965616125113370944-n.jpg",
-      "https://i.ibb.co/pv3FdQrF/Snap-Insta-to-483876399-18491527819001486-1017862629739679861-n.jpg",
-      "https://i.ibb.co/3yPqWtN8/Snap-Insta-to-484144194-18490851487001486-3337812738107921353-n.jpg"
-    ],
-    correctWord: "Sheeny Velasco",
-    socialMediaUsername: "@sheenyberry",
-    socialMediaLink: "https://getallmylinks.com/sheenyberry",
-    placeholder: "Enter your guess (Full Name)"
-  },
-  "2025-05-07": {
-    images: [
-      "https://i.ibb.co/wr80W70r/Snap-Insta-to-485451010-18494085925036863-5000970990621119072-n.jpg",
-      "https://i.ibb.co/8Dj0B2fD/Snap-Insta-to-357996497-1673912149714604-3976168879462412990-n.jpg",
-      "https://i.ibb.co/nqbmwtqq/Snap-Insta-to-471850155-575563561911405-8411403499432906787-n.jpg"
-    ],
-    correctWord: "Alyssa Eckenroth",
-    socialMediaUsername: "@alyssaeckenroth",
-    socialMediaLink: "https://www.instagram.com/alyssaeckenroth/",
-    placeholder: "Enter your guess (Full Name)"
-  },
-  "2025-05-08": {
-    images: [
-      "https://scontent.fmnl37-1.fna.fbcdn.net/v/t39.30808-6/492044556_700175525855081_3743634320227882744_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeEce6ZYZhhoGrRbU8TPV-H8UJcyA8CKkrRQlzIDwIqStEIkrA9wacg7UnLj1QHn-jwSu2vlG6XWJHL6-RrhwhXH&_nc_ohc=_4X8rbsvtY0Q7kNvwGbCPPp&_nc_oc=AdkAKjPSWGir00h4KiSMQo2HwcIxwPWoJiLaW0NiRfOx55IQuZ4h7wNnTJAxCjReP5o&_nc_zt=23&_nc_ht=scontent.fmnl37-1.fna&_nc_gid=qxqJjE1edBNh4jH3yLjUHw&oh=00_AfG4WMUH7B5CRDo1OI5i0cR6jGGb5GyGFjxhoF6kJ8Txpw&oe=6818CE15",
-      "https://i.ibb.co/sJJDnZQj/Snap-Insta-to-475308667-18327633814233629-2834385034515290182-n.jpg",
-      "https://i.ibb.co/Fqf0mg0k/Snap-Insta-to-463596978-421964717361631-5911334653358302529-n.jpg"
-    ],
-    correctWord: "Jennie",
-    socialMediaUsername: "@jennierubyjane",
-    socialMediaLink: "https://www.instagram.com/jennierubyjane/?hl=en",
-    placeholder: "Enter your guess (First Name)"
-  },
-  "2025-05-09": {
-    images: [
-      "https://pbs.twimg.com/media/GceW5mzXAAAWLO2?format=jpg&name=large"
-    ],
+  "2025-06-19": {
+    images: ["https://pbs.twimg.com/media/GceW5mzXAAAWLO2?format=jpg&name=large"],
     correctWord: "Violet Myers",
     socialMediaUsername: "@waifuviolet",
     socialMediaLink: "https://www.instagram.com/waifuviolet/?hl=en",
-    placeholder: "Enter your guess (Full Name)"
+    placeholder: "Enter your guess"
   },
-  "2025-05-10": {
-    images: [
-      "https://pbs.twimg.com/media/Ger7CrpXYAAbdgw?format=jpg&name=large",
-      "https://i.ibb.co/r2wF4yxs/Snap-Insta-to-482621388-18164439751336926-223906374140538726-n.jpg",
-      "https://i.ibb.co/vvkYtNYW/Snap-Insta-to-457396907-3914524452201907-7194868956224132042-n.jpg"
-    ],
+  "2025-06-20": {
+    images: ["https://pbs.twimg.com/media/Ger7CrpXYAAbdgw?format=jpg&name=large"],
     correctWord: "Ella Netzer",
     socialMediaUsername: "@ella_netzer8",
     socialMediaLink: "https://www.instagram.com/ella_netzer8/",
-    placeholder: "Enter your guess (Full Name)"
-  }
-  // Add more dates as needed
+    placeholder: "Enter your guess"
+  },
+  "2025-06-21": {
+    images: ["https://pbs.twimg.com/media/GooRwDwXMAAKjzs?format=jpg&name=large"],
+    correctWord: "Sabrina Carpenter",
+    socialMediaUsername: "@sabrinacarpenter",
+    socialMediaLink: "https://www.instagram.com/sabrinacarpenter/",
+    placeholder: "Enter your guess"
+  },
+  "2025-06-22": {
+    images: ["https://pbs.twimg.com/media/Gkk--H_bkAAHvA0?format=jpg&name=large"],
+    correctWord: "Antje Utgaard",
+    socialMediaUsername: "@awesomeantjay",
+    socialMediaLink: "https://www.instagram.com/awesomeantjay/",
+    placeholder: "Enter your guess"
+  },
+  "2025-06-23": {
+    images: ["https://pbs.twimg.com/media/GoSdSd5XIAAC06I?format=jpg&name=4096x4096"],
+    correctWord: "Duygu German",
+    socialMediaUsername: "@duygu.grmn",
+    socialMediaLink: "https://www.instagram.com/duygu.grmn/",
+    placeholder: "Enter your guess"
+  },
+  "2025-06-24": {
+    images: ["https://pbs.twimg.com/media/GoloZ5fWAAA02if?format=jpg&name=4096x4096"],
+    correctWord: "Sydney Sweeney",
+    socialMediaUsername: "@sydney_sweeney",
+    socialMediaLink: "https://www.instagram.com/sydney_sweeney/",
+    placeholder: "Enter your guess"
+  },
+  "2025-06-25": {
+    images: ["https://pbs.twimg.com/media/GoZ_XF2XoAA-Wi_?format=jpg&name=900x900"],
+    correctWord: "Ashley Schultz",
+    socialMediaUsername: "@ashxschultz",
+    socialMediaLink: "https://www.instagram.com/ashxschultz/",
+    placeholder: "Enter your guess"
+  },
+  "2025-06-26": {
+    images: ["https://pbs.twimg.com/media/GoZ_hcqWcAAsYiu?format=jpg&name=large"],
+    correctWord: "Hilda Sigurdsson",
+    socialMediaUsername: "@hildasigurdsson",
+    socialMediaLink: "https://www.instagram.com/hildasigurdsson/",
+    placeholder: "Enter your guess"
+  },
+  "2025-06-27": {
+    images: ["https://pbs.twimg.com/media/GoUCt48XEAA3zZ0?format=jpg&name=4096x4096"],
+    correctWord: "Lil Nyachty",
+    socialMediaUsername: "@lilnyachtyy",
+    socialMediaLink: "https://www.instagram.com/lilnyachtyy/",
+    placeholder: "Enter your guess"
+  },
+  "2025-06-28": {
+    images: ["https://pbs.twimg.com/media/GomcbUTWMAAhIWA?format=jpg&name=large"],
+    correctWord: "Sydney Thomas",
+    socialMediaUsername: "@itssydneythomas",
+    socialMediaLink: "https://www.instagram.com/iamsydneythomas/",
+    placeholder: "Enter your guess"
+  },
+  "2025-06-29": {
+    images: ["https://pbs.twimg.com/media/GomrMNjXMAAatx1?format=jpg&name=large"],
+    correctWord: "mochiopia",
+    socialMediaUsername: "@mochiopia",
+    socialMediaLink: "https://www.mochiopia.com/",
+    placeholder: "Enter your guess"
+  },
+  "2025-06-30": {
+    images: ["https://lh3.googleusercontent.com/pw/AP1GczNgrV4e4ijXrujMM1Yf4U2MsFIQe2vtRhG5IY_pF_WoRwhTdQS6izaMpDcWo9UbkeSbOKUEvDyaB2HkjZOh6q4yvzVod29DxhsHqcBrv7xPYT6esA1WNo2hSUVYFhWJGcWAcgFE7Ssxy7b4n3jqvaA=w611-h815-s-no-gm"],
+    correctWord: "Sasha Ferro",
+    socialMediaUsername: "@sashaferro",
+    socialMediaLink: "https://www.instagram.com/sashaferro/",
+    placeholder: "Enter your guess"
+  },
+  "2025-07-01": {
+    images: ["https://pbs.twimg.com/media/Gs5YK5TXIAELyGa?format=png&name=900x900"],
+    correctWord: "Katarina Deme",
+    socialMediaUsername: "@katarina.deme",
+    socialMediaLink: "https://www.instagram.com/katarina.deme/?hl=en",
+    placeholder: "Enter your guess"
+  },
 };
 
-// Helper function to get today's game data
+// Helper functions
 export function getTodaysGameData(): GameData {
   const today = new Date();
-  const utcDate = new Date(Date.UTC(today.getUTCFullYear(), today.getUTCMonth(), today.getUTCDate()));
-  const todayStr = utcDate.toISOString().split('T')[0]; // Format: YYYY-MM-DD
-  return gameSchedule[todayStr] || gameSchedule["2025-04-27"]; // Fallback to default if no schedule for today
+  const todayStr = today.toISOString().split('T')[0];
+  return gameSchedule[todayStr] || null;
 }
 
-// Helper function to get yesterday's correct word
 export function getYesterdaysCorrectWord(): string {
   const yesterday = new Date();
-  yesterday.setUTCDate(yesterday.getUTCDate() - 1);
+  yesterday.setDate(yesterday.getDate() - 1);
   const yesterdayStr = yesterday.toISOString().split('T')[0];
-  return gameSchedule[yesterdayStr]?.correctWord || "Violet Myers"; // Fallback to default OR MANUAL* IF NEW STRINGS
-} 
+  return gameSchedule[yesterdayStr]?.correctWord || "Unknown";
+}
